f514af3c3bb757a69365ca9588f6a6846e118949
rmedcraft <rwtmedcraft@gmail.com>
2024-09-10 13:04:13 -0400

    First C code

---
+++ first.c
@@ -0,0 +1,18 @@
+#include <unist.h>
+#include <fcntl.h>
+#include <sys/types.h>
+int main()
+{
+    char buffer[19];
+    int file = 0;
+    if ((file = open("opsys-1.tst", O_RDONLY)) < -1)
+        return 1;
+    if (read(file, buffer, 19) != 19)
+        return 1;
+    printf("%s\n", buffer);
+    if (lseek(file, 10, SEEK_SET) < 0)
+        return 1;
+    if (read(file, buffer, 19) != 19)
+        return 1' printf("%s\n", buffer);
+    return 0;
+}

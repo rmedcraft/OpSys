a3719cd87f1c45dec9d3d9b714de6ea7c50eb147
rmedcraft <rwtmedcraft@gmail.com>
2024-11-15 18:52:56 -0500

    hw7 done?

--- hw7.sh
+++ hw7.sh
@@ -1,4 +1,26 @@
 sudo useradd garfield
-echo "Computer Science & Math\nUsername: rowan, Clone: garfield\n11/14/2024\nCPSC340, Fall 2024" > file1_11-14-24.txt
+# add garfield and rowan to the same group
+sudo groupadd users
+sudo adduser rowan users
+sudo adduser garfield users
+
+echo "Computer Science & Math | Username: rowan, Clone: garfield | 11/14/2024 | CPSC340, Fall 2024" > file1_11-14-24.txt
 ls -l
-chmod +r-w-x file1_11-14-24
+sudo chmod g+r-wx file1_11-14-24.txt
+ls -l
+sudo chmod g+rw-x file1_11-14-24.txt
+
+# make a link to the garfield user
+ln -s ~/Documents/Code/OpSys/file1_11-14-24.txt /home/garfield/file1.txt
+
+su --login garfield
+# try to modify the file1_11-14-24.txt file
+
+sudo chmod g-rwx file1_11-14-24.txt
+
+umask 077
+touch file2_11-14-2024.txt
+
+su --login rowan
+chown rowan file1_11-14-24.txt
+
